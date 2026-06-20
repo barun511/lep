de30b062fe0bd60786e51b9f7d953e6672f7de6c
Hong Ai Ling <hong.ailing.0035@gmail.com>
2026-06-20 23:17:08 +0100

    test

--- elixir/jun_26/test/jun26_test.exs
+++ elixir/jun_26/test/jun26_test.exs
@@ -1,7 +1,15 @@
 defmodule Jun26Test do
   use ExUnit.Case
-  test "greets the world" do
+  doctest Jun26
+
+  test "hello pls fix" do
     board = Jun26.initBoard([[0, 0], [0, 0]])
     assert board == [[0, 0], [0, 0]]
+
+    board = Jun26.initBoard([[0, 1], [0, 1]])
+    assert board == [[0, 1], [0, 1]]
+
+    board = Jun26.initBoard([[0, 2], [0, 2]])
+    assert board == [[0, 2], [0, 2]]
   end
 end
