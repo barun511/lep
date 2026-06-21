7a703d243f69c9f92e2d18c1fb07a53e118787c4
Barun Parruck <barun.parruck@gmail.com>
2026-06-21 15:47:15 +0100

    Tick tcock

--- haskell/jun26/test/Main.hs
+++ haskell/jun26/test/Main.hs
@@ -8,6 +8,7 @@ main :: IO ()
 main = do
   testCase
   testInitializeBoard
+  testTickBoardOnce
 
 testCase :: Assertion
 testCase = do
@@ -17,5 +18,14 @@ testInitializeBoard :: Assertion
 testInitializeBoard = do
   assertEqual "Can initialize board" (initializeBoard testBoard) testBoard
 
+testTickBoardOnce :: Assertion
+testTickBoardOnce = do
+    let board = initializeBoard testBoard
+    let new_board = tickBoard board
+    assertEqual "One tick must die" new_board testEmptyBoard
+
 testBoard :: Vector (Vector Integer)
 testBoard = fromList [fromList [0, 0, 0], fromList [0, 1, 0], fromList [0, 0, 0]]
+
+testEmptyBoard :: Vector (Vector Integer)
+testEmptyBoard = fromList [fromList [0, 0, 0], fromList [0, 0, 0], fromList [0, 0, 0]]
