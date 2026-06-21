ef925c4cfb5610e0cbca211d4d9849f2127a5af8
Barun Parruck <barun.parruck@gmail.com>
2026-06-21 15:31:51 +0100

    Add test

--- haskell/jun26/test/Main.hs
+++ haskell/jun26/test/Main.hs
@@ -1,12 +1,21 @@
 module Main (main) where
-
 import MyLib (game)
-import Test.HUnit (Assertion, assertEqual)
+import Test.HUnit (assertEqual, Assertion)
+import Data.Vector
 
 main :: IO ()
 main = do
-  testCase
+    testCase
+    testInitializeBoard
 
 testCase :: Assertion
 testCase = do
-  assertEqual "Test that game is equal" game "hello world"
+    assertEqual "Test that game is equal" game "hello world"
+
+testInitializeBoard :: Assertion
+testInitializeBoard = do
+    assertEqual "Can initialize board" (initializeBoard testBoard) testBoard
+
+
+testBoard :: Vector (Vector Integer)
+testBoard = fromList [fromList [0,0,0], fromList [0,1,0], fromList [0,0,0]]
