a0a501eedb11eeb125806b9e3e72f865f3e93489
Hong Ai Ling <hong.ailing.0035@gmail.com>
2026-06-23 21:51:21 +0100

    fix

--- haskell/jun26/src/MyLib.hs
+++ haskell/jun26/src/MyLib.hs
@@ -12,10 +12,9 @@ game = "hello world"
 initializeBoard :: State (Vector (Vector Integer)) (Vector (Vector Integer))
 initializeBoard = get
 
-tickBoard :: State (Vector (Vector Integer)) (Vector (Vector Integer))
+tickBoard :: State (Vector (Vector Integer)) ()
 tickBoard = do
   put testEmptyBoard
-  return testEmptyBoard
 
 testEmptyBoard :: Vector (Vector Integer)
 testEmptyBoard = fromList [fromList [0, 0, 0], fromList [0, 0, 0], fromList [0, 0, 0]]
